9c848d252acfbbfbac9f41f4219635417bf96d69
HimaniShirsat <145737201+HimaniShirsat@users.noreply.github.com>
2023-10-09 09:11:41 +0530

    basics of Data Types

---
+++ Himani.java
@@ -0,0 +1,31 @@
+public class Himani {
+
+    public static void main(String args[]){
+        byte b = 23;
+        System.out.println(b);
+        int i = 12;
+        System.out.println(i);
+        long l = 16;
+        System.out.println(l);
+        short s = 32767;
+        System.out.println(s);
+
+        float f = 763737f;
+        System.out.println(f);
+
+        double d = 56.2356;
+        System.out.println(d);
+        char c = 'A';
+        System.out.println(c);
+
+        boolean bool = true;
+        System.out.println(bool);
+        
+        
+        
+        
+
+
+    }
+    
+}
---
+++ datatype.java
@@ -0,0 +1,19 @@
+public class datatype {
+    public static void main(String args[]){
+        byte b=127; 
+        // b;
+        System.out.println(++b);
+        int a=23;
+        long l=16;
+        short s=12;
+
+        float f=2.3f;
+        double d=67.7676;
+
+        char c='a';
+        boolean bool=false;
+
+
+    }
+    
+}
